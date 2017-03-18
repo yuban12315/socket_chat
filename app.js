7742117cbf665bcc0042b7d2f412aeea9e14b1aa
yuban12315 <fengyulin@imudges.com>
2017-03-18 12:39:26 +0800

    build

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ let config = require('./model/config')
 let index = require('./routes/index')
 let users = require('./routes/users')
 
-let app = express();
+let app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -37,20 +37,20 @@ app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+    let err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
 
 // error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
     // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+    res.status(err.status || 500)
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
